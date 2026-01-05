86e5d06473aa63ceb85e3a2742d31723f6c2df15
CodexAlG <codexalg@gmail.com>
2026-01-05 14:24:02 -0700

    style: Remove body background image and update global and layout background colors to a darker black.

--- codice-zero/src/components/tierlist/TierListLayout.jsx
+++ codice-zero/src/components/tierlist/TierListLayout.jsx
@@ -2,7 +2,7 @@
 
 export default function TierListLayout({ children }) {
     return (
-        <div className="min-h-screen bg-[#020617] text-white flex flex-col p-6 overflow-hidden relative">
+        <div className="min-h-screen bg-[#09090b] text-white flex flex-col p-6 overflow-hidden relative">
             {/* Background Grid */}
             <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] pointer-events-none"></div>
 
