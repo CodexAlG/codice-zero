770e647028e93956c60bf3f6e13f45b361c5a410
CodexAlG <codexalg@gmail.com>
2026-02-15 02:48:22 -0700

    feat: add initial `weapons.js` data file containing W-Engine details, including S and A rank Anomaly weapons.

--- codice-zero/src/data/weapons.js
+++ codice-zero/src/data/weapons.js
@@ -1935,6 +1935,52 @@ export const weapons = [
     },
     image: "/CodiceZero/Armas/Soporte/W-Engine-Thougtbop.webp"
   },
+  {
+    id: 5542,
+    name: "Arma de Prueba de Nangong Yu",
+    rank: "S",
+    rol: "Aturdidor",
+    leak: "Beta",
+    detailStats: {
+      baseAtk: { min: 48, max: 713 },
+      subStat: { name: "Tasa de Anomalía", min: "12%", max: "30%" }
+    },
+    effect: {
+      title: "",
+      description: "La Maestría de Anomalía del portador aumenta en 90 puntos; cuando la (Técnica Especial EX) o el (Ataque Básico) del portador infligen Daño Etéreo, el daño infligido por todo el equipo aumenta en 15%, con una duración de 40 segundos, acumulable hasta un máximo de 2 capas, se activa como máximo una vez dentro del mismo movimiento, al activarse de nuevo se reinicia la duración, al poseer 2 capas del efecto, la Maestriía de Anomalía del portador aumenta adicionalmente en 60%, este efecto es único para todo el equipo.",
+      refinements: [ // Tabla de Mejora (R1 - R5)
+        { level: 1, MaestríaAnomalía1: "90", Daño: "15%", MaestríaAnomalía2: "60" },
+        { level: 2, MaestríaAnomalía1: "", Daño: "", MaestríaAnomalía2: "" },
+        { level: 3, MaestríaAnomalía1: "", Daño: "", MaestríaAnomalía2: "" },
+        { level: 4, MaestríaAnomalía1: "", Daño: "", MaestríaAnomalía2: "" },
+        { level: 5, MaestríaAnomalía1: "", Daño: "", MaestríaAnomalía2: "" }
+      ]
+    },
+    image: ""
+  },
+  {
+    id: 5543,
+    name: "Arma de Prueba de Cissia",
+    rank: "S",
+    rol: "Ataque",
+    leak: "Beta",
+    detailStats: {
+      baseAtk: { min: 48, max: 713 },
+      subStat: { name: "Recuperación de Energía", min: "24%", max: "60%" }
+    },
+    effect: {
+      title: "",
+      description: "La Probablidad Crítica aumenta un 20%; cuando el portador consume 20 puntos de energía en una sola acción, por cada 20 puntos de energía consumidos, obtiene un efecto de mejora de 3 segundos: al infligir Daño Eléctrico ignora un 30% de la defensa del objetivo; al obtenerse repetidamente, se extiende la duración, hasta un máxmimo de 30 segundos; al entrar en estado de combate, obtiene 10 segundos de este efecto de mejora; cuando el portador no es el personaje en uso, la duración no disminuye.",
+      refinements: [ // Tabla de Mejora (R1 - R5)
+        { level: 1, ProbablidadCrit: "20%", DañoDef: "30%" },
+        { level: 2, ProbablidadCrit: "23%", DañoDef: "34.5%" },
+        { level: 3, ProbablidadCrit: "26%", DañoDef: "39%" },
+        { level: 4, ProbablidadCrit: "29%", DañoDef: "43.5%" },
+        { level: 5, ProbablidadCrit: "32%", DañoDef: "48%" }
+      ]
+    },
+    image: ""
+  },
 ];
 
 // Función para obtener armas por rango
