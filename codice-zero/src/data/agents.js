f866caf01e5984c352ee7cd668d64f0558b40638
CodexAlG <codexalg@gmail.com>
2026-03-01 20:26:54 -0700

    feat: Add Cissia and Nangong Yu agents for version 2.7, including Cissia's icon.

--- codice-zero/src/data/agents.js
+++ codice-zero/src/data/agents.js
@@ -10,7 +10,7 @@ export const agents = [
     faction: "Divisiòn de Òrdenes Metropolitanas",
     version: "2.7",
     leak: "Beta",
-    image: "",
+    image: "/CodiceZero/ICONOS/Agent_Cissia_Icon.png",
     customStyle: "scale-[1.00] group-hover:scale-[1.10] object-[center_top]",
   },
   {
