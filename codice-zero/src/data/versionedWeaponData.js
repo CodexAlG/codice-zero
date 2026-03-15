a0efe8e89c31fb70a76ecbc9aa2e3416d2096e65
CodexAlG <codexalg@gmail.com>
2026-03-15 01:50:53 -0700

    Armas Cissia y Nangong v2.7.12

--- codice-zero/src/data/versionedWeaponData.js
+++ codice-zero/src/data/versionedWeaponData.js
@@ -66,6 +66,20 @@ export const versionedWeapons = {
                         { level: 5, MaestríaAnomalía1: "90", Daño: "15%", MaestríaAnomalía2: "60" }
                     ]
                 }
+            },
+            "v2.7.12": {
+                versionLabel: "v2.7.12",
+                effect: {
+                    title: "Demonio del Disco",
+                    description: "La Maestría de Anomalía del portador aumenta en 90 puntos; cuando la (Técnica Especial EX) o el (Ataque Basico) del portador infligen Daño Etéreo, el daño infligido por todos los personajes del equipo aumenta un 15%, con una duración de 40 segundos, acumulable hasta un máximo de 2 capas, se activa como máximo una vez dentro del mismo movimiento, al activarse de nuevo se reinicia la duración, al poseer 2 capas del efecto, la Maestría de Anomalía del portador aumenta adicionalmente en 60 puntos, este efecto es único para todo el equipo.",
+                    refinements: [
+                        { level: 1, MaestríaAnomalía1: "90", Daño: "15%", MaestríaAnomalía2: "60" },
+                        { level: 2, MaestríaAnomalía1: "103", Daño: "17%", MaestríaAnomalía2: "69" },
+                        { level: 3, MaestríaAnomalía1: "117", Daño: "19.5%", MaestríaAnomalía2: "78" },
+                        { level: 4, MaestríaAnomalía1: "130", Daño: "21%", MaestríaAnomalía2: "87" },
+                        { level: 5, MaestríaAnomalía1: "145", Daño: "24%", MaestríaAnomalía2: "96" }
+                    ]
+                }
             }
         }
     },
@@ -99,6 +113,20 @@ export const versionedWeapons = {
                         { level: 3, ProbablidadCrit: "32.5%", DañoDef: "39%" },
                         { level: 4, ProbablidadCrit: "36.3%", DañoDef: "43.5%" },
                         { level: 5, ProbablidadCrit: "40%", DañoDef: "48%" }
+                    ]
+                }
+            },
+            "v2.7.12": {
+                versionLabel: "v2.7.12",
+                effect: {
+                    title: "Toxina Biomimética",
+                    description: "La Probablidad Crítica aumenta un 25%; cuando el portador consume 20 puntos de energía en una sola acción, por cada 20 puntos de energía consumidos, obtiene un efecto de mejora de 3 segundos: al infligir Daño Eléctrico ignora un 28% de la defensa del objetivo; al obtenerse repetidamente, se extiende la duración, hasta un máxmimo de 30 segundos; al entrar en estado de combate, obtiene 10 segundos de este efecto de mejora; cuando el portador no es el personaje en uso, la duración no disminuye.",
+                    refinements: [
+                        { level: 1, ProbablidadCrit: "25%", DañoDef: "28%" },
+                        { level: 2, ProbablidadCrit: "28.8%", DañoDef: "31.5%" },
+                        { level: 3, ProbablidadCrit: "32.5%", DañoDef: "35%" },
+                        { level: 4, ProbablidadCrit: "36.3%", DañoDef: "38.5%" },
+                        { level: 5, ProbablidadCrit: "40%", DañoDef: "42%" }
                     ]
                 }
             }
