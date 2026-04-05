fa57d4b5d094895ade313f97af8d33f86c21f643
CodexAlG <codexalg@gmail.com>
2026-04-05 01:35:31 -0700

    feat: update agent skills and weapon effects for improved gameplay mechanics

--- codice-zero/src/data/weapons.js
+++ codice-zero/src/data/weapons.js
@@ -2190,16 +2190,16 @@ export const weapons = [
       subStat: { name: "Tasa de Anomalía", min: "12%", max: "30%" }
     },
     effect: {
-      title: "Efecto del Arma de Prueba de Promeia",
-      description: "El Daño Hielo infligido por el portador aumenta un 35%; cuando el portador activa la (Técnica Especial EX) e inflige Daño Hielo, el daño de Anomalía de Atributo infligido por sí mismo aumenta un 15%, con una duración de 30 segundos, acumulable hasta un máximo de 2 capas, se activa como máximo una vez dentro del mismo movimiento, al activarse de nuevo se reinicia la duración.",
-      refinements: [
-        { level: 1, ProbablidadCrit: "35%", DañoDef: "15%" },
-        { level: 2, ProbablidadCrit: "41%", DañoDef: "16.5%" },
-        { level: 3, ProbablidadCrit: "47%", DañoDef: "18%" },
-        { level: 4, ProbablidadCrit: "53%", DañoDef: "19.5%" },
-        { level: 5, ProbablidadCrit: "60%", DañoDef: "21%" }
-      ]
-    },
+                    title: "Efecto del Arma de Prueba de Promeia",
+                    description: "El Daño Hielo infligido por el portador aumenta un {VALOR_1}; cuando un portador de Atributo Hielo activa la (Técnica Especial EX), el daño de Anomalía de Atributo y el daño de (Desorden) infligidos por sí mismo aumentan un {VALOR_2}, con una duración de 40 segundos, acumulable hasta un máximo de 2 capas, se activa como máximo una vez dentro del mismo movimiento, al activarse de nuevo se reinicia la duración, al poseer 2 capas del efecto, el daño de (Abloom) infligido por el personaje aumenta adicionalmente un {VALOR_3}.",
+                    refinements: [
+                        { level: 1, ProbablidadCrit: "35%", DañoDef: "7.5%" , DañoAbloom: "25%"},
+                        { level: 2, ProbablidadCrit: "41%", DañoDef: "8.5", DañoAbloom: "28.7" },
+                        { level: 3, ProbablidadCrit: "47%", DañoDef: "9.5%", DañoAbloom: "32.5" },
+                        { level: 4, ProbablidadCrit: "53%", DañoDef: "10.5%", DañoAbloom: "36.2", },
+                        { level: 5, ProbablidadCrit: "60%", DañoDef: "11.5%", DañoAbloom: "40%" }
+                    ]
+                },
     image: "/CodiceZero/Armas/Anomalia/W-Engine_de_Prueba_de_Promeia.webp",
     exclusiveAgent: {
       name: "Promeia",
